31e555d3a52ab3079ec5a85fd726683752b8ed10
arvhol <85239020+arvhol@users.noreply.github.com>
2022-02-22 15:13:50 +0100

    k

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -7,19 +7,23 @@ public class HTTPAsk {
     public static void main( String[] args) {
         // Your code here
         int port = 0;
-        String str = "HelloWorld, connected";
+        String str = "HelloWorld, connected?";
         byte[] arr = str.getBytes();
+        System.out.println(str + " ok");
+
+        // read port num from arg line
+        port =  Integer.parseInt(args[0]);
+        System.out.println(port);
 
         try {
-            // read port num from arg line
-            port =  Integer.parseInt(args[0]);
             // creates a server socket that listens to port "portnum"
             ServerSocket welcomeSocket = new ServerSocket(port);
+            System.out.println(port+1);
+
             while(true) {
                 Socket connectionSocket = welcomeSocket.accept();
-                connectionSocket.getOutputStream().write(arr);
+                connectionSocket.close();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
