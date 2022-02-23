e9e6fec8d1e39acf2ce719fe9e12653e7e2f81bc
arvhol <85239020+arvhol@users.noreply.github.com>
2022-02-23 11:21:51 +0100

    k

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -15,6 +15,14 @@ public class HTTPAsk {
         port =  Integer.parseInt(args[0]);
         System.out.println(port);
 
+        // testar buffer till read
+        int buffSize = 1024;
+        byte[] fromClient = new byte[buffSize];
+        // test
+        int testWhile = 0;
+
+        ByteArrayOutputStream buff = new ByteArrayOutputStream();
+
         try {
             // creates a server socket that listens to port "portnum"
             ServerSocket welcomeSocket = new ServerSocket(port);
@@ -22,7 +30,28 @@ public class HTTPAsk {
 
             while(true) {
                 Socket connectionSocket = welcomeSocket.accept();
+                System.out.println("OK1");
+
+                arr = (str + testWhile).getBytes();
                 connectionSocket.getOutputStream().write(arr);
+                System.out.println("OK2");
+
+                int len = connectionSocket.getInputStream().read(fromClient);
+                System.out.println("OK3");
+                while(len == buffSize) {
+                    buff.write(fromClient);
+                    len = connectionSocket.getInputStream().read(fromClient);
+                    System.out.println("99");
+                    testWhile++;
+                }
+                buff.write(fromClient);
+                System.out.println("fin");
+                byte[] finitMSG = new byte[(buffSize*testWhile) + len];
+                finitMSG = buff.toByteArray();
+                connectionSocket.getOutputStream().write("almost".getBytes());
+                connectionSocket.getOutputStream().write(finitMSG);
+                connectionSocket.getOutputStream().write("fin".getBytes());
+
                 connectionSocket.close();
             }
         } catch (IOException e) {
