a085501c619184f739e12988355923887bb31226
arvhol <85239020+arvhol@users.noreply.github.com>
2022-02-27 14:56:43 +0100

    k

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -4,22 +4,48 @@ import tcpclient.*;
 
 public class HTTPAsk {
     // main is server
-    byte[] fromClient = new byte[1024];
 
-    // TCPClient params
-    String hostname = null;
-    Integer clientPort = null;
-    byte[] clientMsg = null;
-    // Optional TCPClient params
-    Integer limit = null;
-    Integer timeout = null;
-    boolean shutdown = false;
+    public static void main(String[] args) {
 
-    public static void main( String[] args) {
+        // TCPClient params
+        String hostname = null;
+        Integer clientPort = null;
+        byte[] clientMsg = null;
+
+        // Optional TCPClient params
+        Integer limit = null;
+        Integer timeout = null;
+        boolean shutdown = false;
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int serverPort = 0;
+        byte[] fromClient = new byte[1024];
+
         try {
             // Your code here
-            int serverPort = Integer.parseInt(args[0]);
+            // binds a socket to serverPort
+            serverPort = Integer.parseInt(args[0]);
             ServerSocket welcomeSocket = new ServerSocket(serverPort);
+
+            while(true) {
+                Socket connectionSocket = welcomeSocket.accept();
+
+                int fromClientLength = connectionSocket.getInputStream().read(fromClient);
+
+                // Compute response
+                buffer.write(fromClient, 0, fromClientLength);
+                if(buffer.toString().contains("GET /ask?")){
+                    byte[] toClient = buffer.toByteArray();
+                    connectionSocket.getOutputStream().write(toClient);
+                }
+                else{
+                    connectionSocket.getOutputStream().write("ERROR 400 or 404".getBytes());
+                }
+
+                //END
+                buffer.reset();
+                connectionSocket.close();
+            }
 
         } catch (IOException io) {
             io.printStackTrace();
